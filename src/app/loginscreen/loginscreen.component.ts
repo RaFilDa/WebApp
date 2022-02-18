d9b7adb8e1a4f52a2592d7bb2cc1dbcdae13d424
radecek04 <radek.piterak@gmail.com>
2022-02-18 20:40:30 +0100

    Navigation Test Implementation tested navigation implementation

--- src/app/loginscreen/loginscreen.component.ts
+++ src/app/loginscreen/loginscreen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-loginscreen',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginscreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  submit() {
+    this.router.navigate(['/dashboard'])
+  }
 }
