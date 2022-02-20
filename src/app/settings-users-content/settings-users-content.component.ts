f012a694ff88954080a0240aa40ef84abcb203ec
MrazFilip <filda.mraz@volny.cz>
2022-02-20 13:39:06 +0100

    Mráz - Settings and bug fixes done :)

--- src/app/settings-users-content/settings-users-content.component.ts
+++ src/app/settings-users-content/settings-users-content.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface UserData {
+  name: string;
+  email: string;
+}
+
+
+const USER_DATA: UserData[] = [
+  {name: 'admin', email: 'admin@email.com'},
+  {name: 'foo', email: 'foo@email.com'},
+  {name: 'admin3', email: 'admin3@email.com'},
+];
+
+
 @Component({
   selector: 'app-settings-users-content',
   templateUrl: './settings-users-content.component.html',
   styleUrls: ['./settings-users-content.component.css']
 })
+
 export class SettingsUsersContentComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  displayedColumns: string[] = ['name', 'email'];
+  dataSource = USER_DATA;
 
 }
