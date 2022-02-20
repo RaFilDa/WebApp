0be0700b3724c6aa1b54954d6c4cdf6027db5fea
radecek04 <radek.piterak@gmail.com>
2022-02-20 12:08:04 +0100

    Arrow fix Arrow set to end of line, simplified report rendering.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,12 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexModule } from "@angular/flex-layout";
 import { MatListModule } from "@angular/material/list";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatGridListModule } from "@angular/material/grid-list";
+import { MatGridListModule} from "@angular/material/grid-list";
 import { GroupsContentComponent } from './groups-content/groups-content.component';
 import { SettingsContentComponent } from './settings-content/settings-content.component';
 import { SettingsGeneralContentComponent } from './settings-general-content/settings-general-content.component';
 import { SettingsLogsContentComponent } from './settings-logs-content/settings-logs-content.component';
 import { SettingsUsersContentComponent } from './settings-users-content/settings-users-content.component';
-
 
 @NgModule({
   declarations: [
