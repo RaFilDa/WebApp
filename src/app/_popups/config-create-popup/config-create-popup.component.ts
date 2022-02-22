e7fc54347177120fbdb354c87acbb21d7f24f22d
radecek04 <radek.piterak@gmail.com>
2022-02-22 14:44:47 +0100

    Date Select Table - Days In Week

--- src/app/_popups/config-create-popup/config-create-popup.component.ts
+++ src/app/_popups/config-create-popup/config-create-popup.component.ts
@@ -12,8 +12,22 @@ export class ConfigCreatePopupComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  public selectedDays: number[] = [];
+  public days: boolean[] = Array(31).fill(false);
   public typeOfBackup : string = 'Full';
+
+  isToggled(index: number) {
+    return this.days[index];
+  }
+
+  toggleButton(index: number) {
+    this.days[index] = !this.days[index];
+    if(this.days[index])
+      this.selectedDays.push(index+1);
+    else
+      this.selectedDays.splice(this.selectedDays.indexOf(index+1),1)
+    console.log(this.selectedDays);
+  }
 
   widthCalc(): number {
     return (window.innerWidth - (window.innerWidth / 3))
